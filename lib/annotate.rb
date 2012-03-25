5dab67e608de9039279915239dd769271761d148
Jon Frisby <jfrisby@mrjoy.com>
2012-03-24 18:54:50 -0700

    Whoops, infinite loop. >.<
    This may break things in other cases, but fixes Rails 3 when combined with an updated .rake file.

--- lib/annotate.rb
+++ lib/annotate.rb
@@ -15,7 +15,6 @@ module Annotate
   def self.load_tasks
     if File.exists?('Rakefile')
       require 'rake'
-      load 'Rakefile'
       # Rails 3 wants to load our .rake files for us.
       # TODO: selectively do this require on Rails 2.x?
       Dir[File.join(File.dirname(__FILE__), 'tasks', '**/*.rake')].each { |rake| load rake }

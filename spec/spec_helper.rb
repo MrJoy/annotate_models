00188492c97ab5077087855d838d8137ca926ed5
Jon Frisby <jfrisby@mrjoy.com>
2026-03-03 17:22:19 -0800

    `rubocop --auto-correct`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,28 +41,28 @@ end
 
 # Vendored from `files` gem v0.4.0, to update for newer Ruby.
 module Files
-  def self.default_options level = 2
-    {:remove => true, :name => called_from(level)}
+  def self.default_options(level = 2)
+    { remove: true, name: called_from(level) }
   end
 
-  def self.called_from level = 1
+  def self.called_from(level = 1)
     line = caller[level]
     line.gsub!(/^.:/, '') # correct for leading Windows C:
-    File.basename line.split(':').first, ".rb"
+    File.basename line.split(':').first, '.rb'
   end
 
-  def self.create options = default_options, &block
+  def self.create(options = default_options, &block)
     require 'tmpdir'
     require 'fileutils'
 
     name = options[:name]
-    root = Dir::tmpdir
+    root = Dir.tmpdir
 
     # if the user specified a root directory (instead of default Dir.tmpdir)
     if options[:path]
       # then we will create their directory for them (test context-be friendly)
       root = options[:path]
-      FileUtils::mkdir_p(root)
+      FileUtils.mkdir_p(root)
       # if they gave relative path, this forces absolute
       root = File.expand_path(root)
     end
@@ -72,36 +72,37 @@ module Files
   end
 
   # mixin methods
-  def files options = ::Files.default_options   # todo: block
+  # TODO: block
+  def files(options = ::Files.default_options)
     @files ||= ::Files.create(options)
   end
 
-  def file *args, &block
-    files.file *args, &block
+  def file(...)
+    files.file(...)
   end
 
-  def dir *args, &block
-    files.dir *args, &block
+  def dir(...)
+    files.dir(...)
   end
 
   # concrete class for creating files and dirs under a temporary directory
   class Files
     attr_reader :root
 
-    def initialize path, block, options
+    def initialize(path, block, options)
       @root = path
       @dirs = []
       dir nil, &block
-      at_exit {remove} if options[:remove]
+      at_exit { remove } if options[:remove]
     end
 
     # only 1 option supported: 'src'. if specified, is copied into 'name'
-    def dir name, options={}, &block
-      if name.nil?
-        path = current
-      else
-        path = File.join(current, name)
-      end
+    def dir(name, options = {}, &block)
+      path = if name.nil?
+               current
+             else
+               File.join(current, name)
+             end
       FileUtils.mkdir_p path
       @dirs << name if name
 
@@ -111,22 +112,22 @@ module Files
       end
 
       Dir.chdir(path) do
-        instance_eval &block if block
+        instance_eval(&block) if block
       end
       @dirs.pop
       path
     end
 
-    def file name, contents = "contents of #{name}"
+    def file(name, contents = "contents of #{name}")
       if name.is_a? File
         FileUtils.cp name.path, current
-        # todo: return path
+        # TODO: return path
       else
         path = File.join(current, name)
         if contents.is_a? File
           FileUtils.cp contents.path, path
         else
-          file_path = File.open(path, "w") do |f|
+          file_path = File.open(path, 'w') do |f|
             f.write contents
           end
         end
@@ -139,14 +140,14 @@ module Files
     end
 
     private
+
     def current
       File.join @root, *@dirs
     end
-
   end
 end
 
-def Files options = ::Files.default_options, &block
+def Files(options = ::Files.default_options, &block)
   files = ::Files.create options, &block
   files.root
 end
